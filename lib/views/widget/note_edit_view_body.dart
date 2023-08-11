bb8fa2adf73691229c5591d0407d29f3cbd9e7c3
amgadelmarghny <amgadsaad22@gmail.com>
2023-08-11 16:14:05 +0300

    put colorItem & edit note view color list in their own file

--- lib/views/widget/note_edit_view_body.dart
+++ lib/views/widget/note_edit_view_body.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:note_app/app%20constants/app_color.dart';
 import 'package:note_app/bloc/notes_cubit/notes_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/widget/widget%20componant/custom_appdar.dart';
 
-import '../../app constants/app_string.dart';
 import 'widget componant/custom_text_field.dart';
-import 'widget componant/list_view_color.dart';
+import 'widget componant/edit_note_view_color_list.dart';
 
 class NoteEditViewBody extends StatefulWidget {
   const NoteEditViewBody({super.key, required this.noteModel});
@@ -71,41 +69,3 @@ class _NoteEditViewBodyState extends State<NoteEditViewBody> {
   }
 }
 
-class EditNoteViewColorList extends StatefulWidget {
-  const EditNoteViewColorList({super.key, required this.noteModel});
-  final NoteModel noteModel;
-  @override
-  State<EditNoteViewColorList> createState() => _EditNoteViewColorListState();
-}
-
-class _EditNoteViewColorListState extends State<EditNoteViewColorList> {
-  late int isChoice;
-  @override
-  void initState() {
-    isChoice = AppColor.kColor.indexOf(Color(widget.noteModel.color));
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 38 * 3,
-      child: ListView.builder(
-          itemCount: AppColor.kColor.length,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                isChoice = index;
-                widget.noteModel.color = AppColor.kColor[index].value;
-                setState(() {});
-              },
-              child: ColorItem(
-                isActive: isChoice == index,
-                color: AppColor.kColor[index],
-              ),
-            );
-          }),
-    );
-  }
-}
